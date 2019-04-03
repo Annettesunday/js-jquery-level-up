ba88cc211297d7003c7647c9ce71565e1b9525f4
annettesunday <annette.odhiambo@andela.com>
2019-04-03 15:07:09 +0300

    ch(object methods): objects to primitive conversion

--- _OBJECTS/_Object_Methods/js/main.js
+++ _OBJECTS/_Object_Methods/js/main.js
@@ -98,3 +98,132 @@ The value of this is defined at run-time
 
 - When this is accessed inside the arrow function, it is taken from outside
 */
+
+
+
+
+
+
+
+// Object to primitive conversion
+
+/*
+For objects, there's no to-boolean conversion, because all objects are true in a boolean context
+So there is only string and numeric conversion
+*/
+
+
+
+/*
+ To primitive
+
+ When an object is used in the context where primitive is required, for instance, in an alert or
+ mathematical operations, it's converted to a primitive value using ToPrimitive algorithm
+
+ That algorithm allows us to customize the conversion using a special object method
+ Depending on the context, the conversion has a so-called "hint"
+
+ There are three variants:
+
+ */
+
+
+
+//   "string" - When an operation expects a string, for object-to-string conversions, like alert:
+
+
+// output
+alert(obj);
+
+// using object as a property key
+anotherObj[obj] = 123;
+
+
+
+
+// "number" - When an operation expects a number, for object-number conversion like maths
+
+// explicit  conversion
+let num = Number(obj);
+
+// math(except the binary plus)
+let n = +obj; //unary plus
+let delta = date1 - date2;
+
+
+// less/greater comparisons
+
+let greater = user1 > user2;
+
+
+
+// "default"- occurs in rare cases when the operator is not sure what type to expect
+
+/*
+For instance, binary plus + can work both with strings(concatenates them) and numbers(Adds them), so both
+strings and numbers would do. Or when an object is compared using == with a string, number or a symbol
+*/
+
+
+// binary plus 
+let total = car1 + CanvasRenderingContext2D;
+
+// obj == string/number/symbol
+if (user == 1) {};
+
+
+// In practice, all built-in objects except for one case(Date) implement "default" conversion the same way as Number
+
+
+
+/*
+To do the conversion, JS tries to find and call three object methods:
+
+1. Call obj[Symbol.toPrimitive](hint) if the method exists
+2. Otherwise if hint is "string"
+    - try obj.toString() and obj.valueOf() , whatever exists
+3. Otherwise if hint is "number" or "default"
+    - try obj.valueOf() and obj.toString(), whatever exists
+ */
+
+//  Symbol.toPrimitive
+
+
+let user = {
+    name: "John",
+    money: 1000,
+
+    [Symbol.toPrimitive](hint) {
+        alert( `hint: ${hint}` );
+        return hint == "string" ? `{name: "${this.name}"}` : this.money;
+    }
+};
+
+
+// conversions demo
+
+alert(user); //hint: string -> {name: "John"}
+alert(+user); //hint: number -> 1000
+alert(user + 500); //hint: default -> 1500
+
+/* As we can see from the code, user becomes a self-descriptive string or a money amount depending on the conversion. 
+The single method user[Symbol.toPrimitive] handles all conversion cases.
+*/
+
+
+// toString/valueOf
+
+/*
+If there's no Symbol.toPrimitve then Javascript tries to find them and try in the order:
+toString -> valueOf for string hint
+valueOf -> toString otherwise
+*/
+
+
+
+/*
+ToPrimitive and ToString/ToNumber
+
+
+
+*/
